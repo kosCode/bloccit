4097091caee23597da03ceb1a675f7356bfb5f3e
Kevin Kosiorek <kevin.kosiorek@gmail.com>
2016-11-16 15:10:32 +0000

    assign-17-Seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,10 @@
      body: RandomData.random_paragraph
    )
  end
+
+ puts "#{Post.count}"
+ Post.find_or_create_by(title: "A very special post!", body: "A very unique body")
+ puts "#{Post.count}"
  
  puts "Seed finished"
  puts "#{Post.count} posts created"
